3268378b349ade5e54dc8b1c44b4ed65579eb9ce
maros-o <maros.mec@seznam.cz>
2024-01-22 11:27:02 +0100

    auto

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,14 @@ const ScoreTable: React.FC = () => {
         10fastfingers.com
       </a>
       <div className="absolute top-[140px] text-[50px] text-[#29df2c] font-extrabold">
-        AGRP.DEV
+        <span>AGRP</span>.<span>DEV</span>
       </div>
       <div className="container text-center">
         <table className="min-w-full font-consolas text-white/90 text-[18px]">
           <thead className="text-[#29df2c] font-bold text-[36px]">
             <tr>
-              <th className="py-2 px-4 ">Name</th>
-              <th className="py-2 px-4 ">Score</th>
+              <th className="py-2 px-4">Name</th>
+              <th className="py-2 px-4">Score</th>
               <th className="py-2 px-4">Fingers</th>
             </tr>
           </thead>
@@ -30,14 +30,16 @@ const ScoreTable: React.FC = () => {
                 key={index}
                 className={index % 2 === 0 ? "bg-black" : "bg-[#1b1b1b]"}
               >
-                <td className="py-2 px-4 text-center border-r">{row.name}</td>
+                <td className="py-2 px-4 w-1/3 text-center border-r">
+                  {row.name}
+                </td>
                 <td
                   className={`
-                    py-2 px-4 text-center border-r`}
+                    py-2 px-4 text-center border-r w-1/3`}
                 >
                   {row.score}
                 </td>
-                <td className="py-2 px-4 text-center">{row.fingers}</td>
+                <td className="py-2 px-4 text-center w-1/3">{row.fingers}</td>
               </tr>
             ))}
           </tbody>
