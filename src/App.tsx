ee66f44900809555dd42521d86c0e4ee29cda14c
maros-o <maros.mec@seznam.cz>
2024-01-22 11:20:37 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { ScoreData, scores } from "./data";
+
+const ScoreTable: React.FC = () => {
+  const sortedScores = scores.sort((a, b) => b.score - a.score);
+
+  return (
+    <div className="mx-auto bg-black h-screen w-screen flex flex-col items-center justify-center">
+      <a
+        href="https://10fastfingers.com/typing-test/english"
+        target="_blank"
+        className="text-white/90 absolute top-[206px] text-[20px] tracking-wide hover:text-[#29df2c] transition-all duration-300 animate-pulse"
+      >
+        10fastfingers.com
+      </a>
+      <div className="absolute top-[140px] text-[50px] text-[#29df2c] font-extrabold">
+        AGRP.DEV
+      </div>
+      <div className="container text-center">
+        <table className="min-w-full font-consolas text-white/90 text-[18px]">
+          <thead className="text-[#29df2c] font-bold text-[36px]">
+            <tr>
+              <th className="py-2 px-4 ">Name</th>
+              <th className="py-2 px-4 ">Score</th>
+              <th className="py-2 px-4">Fingers</th>
+            </tr>
+          </thead>
+          <tbody className="border">
+            {sortedScores.map((row: ScoreData, index: number) => (
+              <tr
+                key={index}
+                className={index % 2 === 0 ? "bg-black" : "bg-[#1b1b1b]"}
+              >
+                <td className="py-2 px-4 text-center border-r">{row.name}</td>
+                <td
+                  className={`
+                    py-2 px-4 text-center border-r`}
+                >
+                  {row.score}
+                </td>
+                <td className="py-2 px-4 text-center">{row.fingers}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+function App() {
+  return <ScoreTable />;
+}
+
+export default App;
---
+++ src/data.ts
@@ -0,0 +1,21 @@
+export type ScoreData = {
+  name: string;
+  score: number;
+  fingers?: number;
+};
+
+export const scores: ScoreData[] = [
+  { name: "Vojtěch Kosa", score: 121, fingers: 8 },
+  { name: "Christian Krutsche", score: 83, fingers: 7 },
+  { name: "Maroš Mečiar", score: 82, fingers: 3 },
+  { name: "Tomáš Řehák", score: 62 },
+  { name: "Marek Svatoš", score: 92 },
+  { name: "Josef Swaczyna", score: 37, fingers: 7 },
+  { name: "Jiří Herold", score: 48, fingers: 5 },
+  { name: "Dan Stržínek", score: 32, fingers: 2 },
+  { name: "Jiří Krokviak", score: 92, fingers: 20 },
+  { name: "Silvie Staníková", score: 32, fingers: 8 },
+  { name: "Jiří Kadlčík", score: 56 },
+  { name: "Tomáš Věncek", score: 59 },
+  { name: "Zdeněk Růžička", score: 26 },
+];
