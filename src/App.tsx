0abd93916a4803ffffb570c2d9562e56d7ec85b4
maros-o <maros.mec@seznam.cz>
2024-01-22 14:19:31 +0100

    auto

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ScoreData, scores } from "./data";
+import { ScoreData, scores, teamOptions } from "./data";
 
 const TyperTable: React.FC = () => {
   const sortedScores = scores.sort((a, b) => b.score - a.score);
@@ -44,6 +44,54 @@ const TyperTable: React.FC = () => {
   );
 };
 
+const TeamTable: React.FC = () => {
+  const averageScores = teamOptions.map((team) => {
+    const teamScores = scores.filter((score) => score.team === team);
+    const teamScoreSum = teamScores.reduce((acc, cur) => acc + cur.score, 0);
+    const teamAverageScore = teamScoreSum / teamScores.length || 0;
+
+    return {
+      name: team,
+      score: teamAverageScore,
+    };
+  });
+
+  const sortedTeams = averageScores.sort((a, b) => b.score - a.score);
+
+  return (
+    <table className="min-w-full font-consolas text-white/90 text-[18px]">
+      <thead className="text-[#29df2c] font-bold text-[36px] tracking-wide">
+        <tr>
+          <th className="py-2 px-4">RANK</th>
+          <th className="py-2 px-4">TEAM</th>
+          <th className="py-2 px-4">AVG SCORE</th>
+        </tr>
+      </thead>
+      <tbody className="border">
+        {sortedTeams.map((row: ScoreData, index: number) => (
+          <tr
+            key={index}
+            className={index % 2 === 0 ? "bg-black" : "bg-[#1b1b1b]"}
+          >
+            <td className="py-2 px-4 w-[10%] text-center border-r font-bold">
+              {index + 1}.
+            </td>
+            <td className="py-2 px-4 w-[50%] text-center border-r relative">
+              {row.name.toUpperCase()}
+            </td>
+            <td
+              className={`
+                    py-2 px-4 w-[20%] text-center border-r`}
+            >
+              {row.score}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
 function App() {
   return (
     <div className="mx-auto bg-black min-h-screen min-w-screen flex flex-col items-center justify-center flex-grow-1">
@@ -57,8 +105,11 @@ function App() {
       >
         10fastfingers.com
       </a>
-      <div className="container text-center m-[100px]">
+      <div className="container text-center my-[100px]">
         <TyperTable />
+      </div>
+      <div className="container text-center mb-[100px]">
+        <TeamTable />
       </div>
     </div>
   );
